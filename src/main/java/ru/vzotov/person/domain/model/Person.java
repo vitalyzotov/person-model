69adccb0262dd4e70833f4d051fbe93bf1125923
vzotov <vitalyzotov@gmail.com>
2022-01-19 14:56:32 +0400

    Initial implementation

---
+++ src/main/java/ru/vzotov/person/domain/model/Person.java
@@ -0,0 +1,83 @@
+package ru.vzotov.person.domain.model;
+
+import org.apache.commons.lang.Validate;
+import ru.vzotov.ddd.shared.Entity;
+
+import java.util.Objects;
+
+public class Person implements Entity<Person> {
+
+    private PersonId personId;
+
+    /**
+     * First name
+     */
+    private String firstName;
+
+    /**
+     * Last name
+     */
+    private String lastName;
+
+    /**
+     * Display name
+     */
+    private String displayName;
+
+    public Person(PersonId personId, String firstName, String lastName, String displayName) {
+        Validate.notNull(personId);
+        Validate.notEmpty(firstName);
+        Validate.notEmpty(lastName);
+        Validate.notEmpty(displayName);
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.displayName = displayName;
+    }
+
+    public PersonId personId() {
+        return personId;
+    }
+
+    public String firstName() {
+        return firstName;
+    }
+
+    public String lastName() {
+        return lastName;
+    }
+
+    public String displayName() {
+        return displayName;
+    }
+
+    @Override
+    public boolean sameIdentityAs(Person that) {
+        return that != null && Objects.equals(personId, that.personId);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person that = (Person) o;
+        return sameIdentityAs(that);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(personId);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + personId + '}';
+    }
+
+    protected Person() {
+        // for Hibernate
+    }
+
+    private Long id; // surrogate key
+
+}
---
+++ src/main/java/ru/vzotov/person/domain/model/PersonId.java
@@ -0,0 +1,68 @@
+package ru.vzotov.person.domain.model;
+
+import org.apache.commons.lang.Validate;
+import ru.vzotov.ddd.shared.ValueObject;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class PersonId implements ValueObject<PersonId> {
+
+    private static final String PERSON_AUTHORITY_PREFIX = "PERSON_";
+
+    private String value;
+
+    public PersonId(String value) {
+        Validate.notEmpty(value);
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public String authority() {
+        return PERSON_AUTHORITY_PREFIX + value;
+    }
+
+    public static boolean isValidAuthority(String authority) {
+        return authority.startsWith(PERSON_AUTHORITY_PREFIX);
+    }
+
+    public static PersonId fromAuthority(String authority) {
+        if (!isValidAuthority(authority)) throw new IllegalArgumentException();
+        return new PersonId(authority.replace(PERSON_AUTHORITY_PREFIX, ""));
+    }
+
+    public static PersonId nextId() {
+        return new PersonId(UUID.randomUUID().toString());
+    }
+
+    @Override
+    public boolean sameValueAs(PersonId that) {
+        return that != null && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonId that = (PersonId) o;
+        return sameValueAs(that);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    protected PersonId() {
+        // for Hibernate
+    }
+
+}
